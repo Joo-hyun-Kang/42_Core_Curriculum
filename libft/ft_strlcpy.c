6c807db09b72e08125cea355d6108702af1e7320
jokang <autoba9687@gmail.com>
2021-11-26 17:04:21 +0900

    Part1 단위 테스트로 디버깅 및 코딩 표준 고치기 - dstsize 0일 때 dst에 널 문자가 안 붙게 수정

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,23 +6,24 @@
 /*   By: jokang <autoba9687@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 16:19:28 by jokang            #+#    #+#             */
-/*   Updated: 2021/11/17 16:19:29 by jokang           ###   ########.fr       */
+/*   Updated: 2021/11/26 17:03:23 by jokang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
 	while (i + 1 < dstsize && *src != '\0')
 	{
 		*dst++ = *src++;
-		i++;		
+		i++;
 	}	
-	*dst = '\0';
+	if (dstsize != 0)
+		*dst = '\0';
 	while (*src != '\0')
 	{
 		src++;
