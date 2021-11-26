de370f3b96d4d5758c1518537e48d67576a54141
jokang <autoba9687@gmail.com>
2021-11-26 16:05:09 +0900

    Part1 단위 테스트로 디버깅 및 코딩 표준 고치기 - norm 수정

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jokang <autoba9687@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 18:22:27 by jokang            #+#    #+#             */
-/*   Updated: 2021/11/25 19:24:18 by jokang           ###   ########.fr       */
+/*   Updated: 2021/11/26 13:50:44 by jokang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,21 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	i = 0;
 	temp_dst = (char *)dst;
 	temp_src = (char *)src;
-	if (temp_dst > temp_src)
+	if (temp_dst < temp_src)
 	{
-		while (i < len)
+		while (i++ < len)
 		{
 			*temp_dst++ = *temp_src++;
-			i++;
 		}
 	}
 	else
 	{
 		temp_dst += len - 1;
 		temp_src += len - 1;
-		while (i < len)
+		while (i++ < len)
 		{
 			*temp_dst-- = *temp_src--;
-			i++;
 		}
 	}
-	return (dst); 
+	return (dst);
 }
