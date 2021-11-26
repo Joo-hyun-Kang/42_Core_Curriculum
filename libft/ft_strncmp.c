ec2e4689d90e6e134e1e699443a152091a5e4ae2
jokang <autoba9687@gmail.com>
2021-11-26 21:24:10 +0900

    Part1 단위 테스트로 디버깅 및 코딩 표준 고치기 - return value is changed as unsigned char

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,21 +6,26 @@
 /*   By: jokang <autoba9687@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 21:03:03 by jokang            #+#    #+#             */
-/*   Updated: 2021/11/22 15:12:11 by jokang           ###   ########.fr       */
+/*   Updated: 2021/11/26 21:12:46 by jokang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strcmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t i;
+	size_t	i;
 
 	i = 0;
-	while (*s1 != '\0' && *s1 == *s2 && i++ < n)
+	while (i < n)
 	{
+		if (*s1 == '\0' || *s2 == '\0' || *s1 != *s2)
+		{
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		}
+		i++;
 		s1++;
 		s2++;
 	}
-	return (s1 - s2);
+	return (0);
 }
