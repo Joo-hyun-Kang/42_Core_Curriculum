ebcbb997209320fe5ffd3ce864f6e8c98c495962
jokang <autoba9687@gmail.com>
2021-11-26 21:43:07 +0900

    Part1 단위 테스트로 디버깅 및 코딩 표준 고치기 - ft_strrchr 포인터 연산 뒤로 탐색하기 로직 수정

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: jokang <autoba9687@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 15:11:06 by jokang            #+#    #+#             */
-/*   Updated: 2021/11/22 16:19:19 by jokang           ###   ########.fr       */
+/*   Updated: 2021/11/26 21:42:22 by jokang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,10 @@ char	*ft_strrchr(const char *s, int c)
 	char	*p;
 	size_t	len;
 
-
 	ch = (char)c;
 	len = ft_strlen(s);
 	p = (char *)s + len - 1;
-	while (p != s)
+	while (p != s - 1)
 	{
 		if (*p == ch)
 		{
