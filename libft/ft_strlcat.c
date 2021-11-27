6d20ad8955160350af3dfe70b898ebeef1bf4b57
jokang <autoba9687@gmail.com>
2021-11-27 11:42:42 +0900

    Part1 단위 테스트로 디버깅 및 코딩 표준 고치기 - ft_strlcat 리턴값 수정

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jokang <autoba9687@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 16:18:54 by jokang            #+#    #+#             */
-/*   Updated: 2021/11/26 16:50:02 by jokang           ###   ########.fr       */
+/*   Updated: 2021/11/27 11:15:26 by jokang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,28 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
+	size_t	copied_length;
 	size_t	dst_length;
 	size_t	src_length;
 
-	dst_length = 0;
+	copied_length = 0;
+	dst_length = ft_strlen(dst);
 	src_length = ft_strlen(src);
 	while (*dst != '\0')
 	{
 		dst++;
-		dst_length++;
+		copied_length++;
 	}
-	while (dst_length + 1 < dstsize && *src != '\0')
+	while (copied_length + 1 < dstsize && *src != '\0')
 	{
 		*dst++ = *src++;
-		dst_length++;
+		copied_length++;
 	}
 	*dst = '\0';
-	while (*src != '\0')
-	{
-		src++;
-		dst_length++;
-	}
-	if (dst_length < dstsize)
-		return (src_length);
+	while (*src++ != '\0')
+		copied_length++;
+	if (dstsize < dst_length)
+		return (dst_length + src_length);
 	else
-		return (dst_length);
+		return (copied_length);
 }
