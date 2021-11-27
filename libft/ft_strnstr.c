3d2b8ff497e869520f30d49d3129badb84716edb
jokang <autoba9687@gmail.com>
2021-11-27 16:40:51 +0900

    Part1 단위 테스트로 디버깅 및 코딩 표준 고치기 - ft_strnstr 핵심 로직 널 문자 탐색 추가

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -6,34 +6,40 @@
 /*   By: jokang <autoba9687@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 15:13:44 by jokang            #+#    #+#             */
-/*   Updated: 2021/11/27 13:47:59 by jokang           ###   ########.fr       */
+/*   Updated: 2021/11/27 16:36:46 by jokang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	ft_cmpstr(const char *p_hay, const char *p_ne, size_t len)
+{
+	size_t	j;
+
+	j = 0;
+	while (j++ < len && *p_hay != '\0' && *p_hay == *p_ne)
+	{
+		p_hay++;
+		p_ne++;
+	}
+	if (*p_ne == '\0')
+		return (1);
+	else
+		return (0);
+}
+
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	const char	*p_haystack;
-	const char	*p_needle;
 	size_t		i;
-	size_t		j;
 
 	if (*needle == '\0')
 		return ((char *)haystack);
 	i = 0;
 	while (i < len && *haystack != '\0')
 	{
-		if (*haystack == *needle)
+		if (*haystack == *needle && ft_cmpstr(haystack, needle, len - i))
 		{
-			j = i;
-			p_haystack = haystack;
-			p_needle = needle;
-			while (*p_haystack++ == *p_needle++ && j++ < len)
-			{
-			}
-			if (*--p_needle == '\0')
-				return ((char *)haystack);
+			return ((char *)haystack);
 		}
 		i++;
 		haystack++;
