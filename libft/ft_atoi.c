0353675b95d6cd6d769efe97343c711049dd65e8
jokang <autoba9687@gmail.com>
2021-11-27 16:40:16 +0900

    Part1 단위 테스트로 디버깅 및 코딩 표준 고치기 - ft_atoi 19자리 이상 리턴 값 수정

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,29 +6,34 @@
 /*   By: jokang <autoba9687@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 15:14:33 by jokang            #+#    #+#             */
-/*   Updated: 2021/11/25 20:29:22 by jokang           ###   ########.fr       */
+/*   Updated: 2021/11/27 15:40:16 by jokang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	ft_isspace(char ch)
+{
+	if (ch == '\t' || ch == '\n' || ch == '\v'
+		|| ch == '\f' || ch == '\r' || ch == ' ')
+	{
+		return (1);
+	}
+	return (0);
+}
+
 int	ft_atoi(const char *str)
 {
-	int	sign;
-	int	result;
+	int					sign;
+	unsigned long long	result;
 
-	while (*str == '\t' || *str == '\n' || *str == '\v'
-		   || *str == '\f' || *str == '\r' || *str == ' ')
-	{
+	while (ft_isspace(*str))
 		str++;
-	}
 	sign = 1;
 	if (*str == '+' || *str == '-')
 	{
 		if (*str == '-')
-		{
 			sign *= -1;
-		}
 		str++;
 	}
 	result = 0;
@@ -36,6 +41,10 @@ int	ft_atoi(const char *str)
 	{
 		result *= 10;
 		result += *str - '0';
+		if (result > 9223372036854775807 && sign == 1)
+			return (-1);
+		if (result > 9223372036854775807 && sign == -1)
+			return (0);
 		str++;
 	}
 	return (sign * result);
