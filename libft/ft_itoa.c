b99103370295130dbfdec54cf11b42036501d5dc
jokang <autoba9687@gmail.com>
2021-12-05 12:46:13 +0900

    Part2 개발 및 디버깅 - ft_itoa.c intitial commit 1. negative num's exception is unsinged int, 2. putnbr logic is recursive

---
+++ libft/ft_itoa.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jokang <autoba9687@gmail.com>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/05 11:17:12 by jokang            #+#    #+#             */
+/*   Updated: 2021/12/05 12:41:24 by jokang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	put_nbr(unsigned int n, int is_neg, char *out_res, unsigned int pos)
+{
+	if (is_neg == 1)
+	{
+		is_neg = 0;
+		*out_res = '-';
+		--pos;
+		out_res++;
+	}
+	if (pos > 1)
+	{
+		put_nbr(n / 10, is_neg, out_res, pos - 1);
+	}
+	n %= 10;
+	*(out_res + --pos) = n + '0';
+}
+
+char	*ft_itoa(int n)
+{
+	int				is_negative;
+	unsigned int	pos;
+	unsigned int	n_digit;
+	unsigned int	n_abs;
+	char			*res;
+
+	is_negative = 0;
+	if (n < 0)
+	{
+		is_negative = 1;
+		n_abs = (unsigned int)((n + 1) * -1 + 1);
+	} 
+	else
+		n_abs = (unsigned int)n;
+	n_digit = n_abs / 10;
+	pos = 1;
+	while (n_digit > 0)
+	{
+		n_digit /= 10;
+		pos++;
+	}
+	if (is_negative == 1)
+		pos += 1;
+	res = (char *)malloc(sizeof(char) * (pos + 1));
+	if (res == NULL)
+		return (NULL);
+	put_nbr(n_abs, is_negative, res, pos);
+	*(res + pos) = '\0';
+	return (res);
+}
