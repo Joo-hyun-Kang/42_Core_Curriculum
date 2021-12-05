03cf4ec44a046e56485f0eaba80191837a96cf7b
jokang <autoba9687@gmail.com>
2021-12-05 12:55:02 +0900

    Part2 개발 및 디버깅 - ft_itoa.c 25줄 norm을 위해서 pos 구하는 로직 함수로 만듬

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: jokang <autoba9687@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 11:17:12 by jokang            #+#    #+#             */
-/*   Updated: 2021/12/05 12:41:24 by jokang           ###   ########.fr       */
+/*   Updated: 2021/12/05 12:51:40 by jokang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,11 +29,25 @@ void	put_nbr(unsigned int n, int is_neg, char *out_res, unsigned int pos)
 	*(out_res + --pos) = n + '0';
 }
 
+int	get_num_pos(unsigned int n)
+{
+	unsigned int	n_digit;
+	unsigned int	pos;
+
+	n_digit = n / 10;
+	pos = 1;
+	while (n_digit > 0)
+	{
+		n_digit /= 10;
+		pos++;
+	}
+	return (pos);
+}
+
 char	*ft_itoa(int n)
 {
 	int				is_negative;
 	unsigned int	pos;
-	unsigned int	n_digit;
 	unsigned int	n_abs;
 	char			*res;
 
@@ -42,16 +56,10 @@ char	*ft_itoa(int n)
 	{
 		is_negative = 1;
 		n_abs = (unsigned int)((n + 1) * -1 + 1);
-	} 
+	}
 	else
 		n_abs = (unsigned int)n;
-	n_digit = n_abs / 10;
-	pos = 1;
-	while (n_digit > 0)
-	{
-		n_digit /= 10;
-		pos++;
-	}
+	pos = get_num_pos(n_abs);
 	if (is_negative == 1)
 		pos += 1;
 	res = (char *)malloc(sizeof(char) * (pos + 1));
