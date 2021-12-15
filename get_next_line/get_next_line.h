a840b5792348ea3ede05279eb8bacae7b7c51eb1
jokang <autoba9687@gmail.com>
2021-12-15 21:05:38 +0900

    updating

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jokang <autoba9687@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 16:13:40 by jokang            #+#    #+#             */
-/*   Updated: 2021/12/15 17:50:48 by jokang           ###   ########.fr       */
+/*   Updated: 2021/12/15 20:16:12 by jokang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 #include <stdio.h>
 
 char	*get_next_line(int fd);
+size_t	ft_get_line_len(char *srcs);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 
 #endif /* GET_NEXT_LINE_H */
