82a0c02939daf1532167dec94d56c81b347e4e79
jokang <autoba9687@gmail.com>
2021-12-15 21:04:30 +0900

    get_next_line - 버퍼 사이즈 >= 줄바꿈 클 경우 read하지 않고 정적 변수에 있는 것 탐색하게 끔 수정 read에서 제일 마지막 줄인 EOF가 올 때 로직 추가 중복로직 함수로 분리

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jokang <autoba9687@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 16:10:11 by jokang            #+#    #+#             */
-/*   Updated: 2021/12/15 19:17:37 by jokang           ###   ########.fr       */
+/*   Updated: 2021/12/15 20:38:39 by jokang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,42 +23,48 @@ char	*get_next_line(int fd)
 
 	if (fd < 3)
 		return (NULL);
+	
 	if (idx_srcs == 0)
-		srcs = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	p = srcs + idx_srcs;
-
-	// 예외처리 필요
-	if (read(fd, p, BUFFER_SIZE) < -1)
-		return (NULL);
-
-	*(p + BUFFER_SIZE) = '\0';
-
-	res = NULL;
-	while (*p != '\0')
 	{
-		printf("alpabet: %c\n", *p);
-		if (*p == '\n')
-		{
-			line_len = p - srcs + 1;
-			printf("line_len : %zu\n", line_len);
-			res = (char	*)malloc(sizeof(char) * line_len);
-			assert(res != 0);
-			ft_memcpy(res, srcs, line_len);
-			break;
-		}
-		p++;
+		srcs = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		read_return = read(fd, srcs, BUFFER_SIZE);
+		if (read_return < 0)
+			return (NULL);
+		*(srcs + BUFFER_SIZE) = '\0';
 	}
 
-	if (*p == '\0')
+	p = srcs + idx_srcs;
+	line_len = ft_get_line_len(p);
+	printf("%zu", line_len);
+	res = NULL;
+	if (line_len != (size_t)-1)
+	{
+		res = (char	*)malloc(sizeof(char) * line_len);
+		ft_memcpy(res, p, line_len);
+		idx_srcs += line_len;
+	}
+	else 
 	{
 	   free(srcs);
 	   srcs = NULL;
 	   idx_srcs = 0;
 	}
-	else
-		idx_srcs = line_len;	
 
 	return (res);
+}
+
+size_t		ft_get_line_len(char *srcs)
+{
+	char	*tmp;
+
+	tmp = srcs;
+	while (*tmp != '\0')
+	{
+		if (*tmp == '\n' || *tmp == EOF)
+			return (tmp - srcs + 1);
+		tmp++;
+	}
+	return ((size_t)-1);
 }
 
 int	main(void)
@@ -76,5 +82,14 @@ int	main(void)
 	line = get_next_line(fd);
 	printf("output : %s\n", line);
 	
+	line = get_next_line(fd);
+	printf("output : %s\n", line);
+
+	line = get_next_line(fd);
+	printf("output : %s\n", line);
+	
+	line = get_next_line(fd);
+	printf("output : %s\n", line);
+
 	close(fd);
 }
