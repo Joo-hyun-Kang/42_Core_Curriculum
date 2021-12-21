67171347b3d67cbdf43257a1019371e5bd9e5402
jokang <autoba9687@gmail.com>
2021-12-21 17:45:07 +0900

    issue_refactoring

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jokang <autoba9687@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 16:10:11 by jokang            #+#    #+#             */
-/*   Updated: 2021/12/21 12:03:40 by jokang           ###   ########.fr       */
+/*   Updated: 2021/12/21 17:44:49 by jokang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,57 +14,70 @@
 
 char	*get_next_line(int fd)
 {
-	static char		*srcs;
-	static size_t	idx_srcs = 0;
+	static char		*buffer;
+	static size_t	idx = 0;
 	char			*p;
 	char			*res;
 	size_t			line_len;
 	int				is_line;
-	int				read_return;
 	char			*tmp;
+	size_t			rotation;
+	size_t			buffer_length;
+	size_t			i;
+	//줄바꿈이 나올 때까지 read한다
+		//static은 배열의 2배로 잡는다
+	//줄바꿈이 나오면 return 한다
+	//EOF가 나오면 return 한다
+	//줄바꿈이 나오지 않으면 다시 나머지 버퍼를 채운다
+	//없으면 반복한다
 	
-	if (idx_srcs == 0)
-	{
-		srcs = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-		read_return = read(fd, srcs, BUFFER_SIZE);
-		if (read_return < 0)
-			return (NULL);
-		*(srcs + BUFFER_SIZE) = '\0';
-	}
+	rotation = 1;
+	is_line = FALSE;
+	buffer_length = BUFFER_SIZE * rotation;
+	buffer = (char *)malloc(sizeof(char) * (buffer_length * 2) + 1));
+	if (read(fd, buffer, buffer_length) < 0)
+		return (NULL);
+	*(buffer + buffer_length) = '\0';
 
-	line_len = 0;
-	p = srcs + idx_srcs;
-	is_line = ft_try_get_line_len(p, &line_len);
-	printf("lien_len : %zu\n", line_len);
-	res = NULL;
-
-	if (is_line == TRUE)
+	while(TRUE)
 	{
-		res = (char	*)malloc(sizeof(char) * line_len + idx_srcs);
-		ft_memcpy(res, srcs, line_len + idx_srcs);
-		idx_srcs = 0;
-		free(srcs);
-	}
-	else 
-	{
-		tmp = (char *)malloc(sizeof(char) * (line_len + idx_srcs + BUFFER_SIZE));
-		assert(tmp != NULL);
-		if (tmp != NULL)
+		i = 0;
+		while (i < 2)
 		{
-			ft_memcpy(tmp, srcs, line_len + idx_srcs - 1);
-			free(srcs);
-			srcs = tmp;
-			tmp += line_len - 1;
-			//다시 읽어오는 것 코드 중복 제거 하기
-			read_return = read(fd, tmp, BUFFER_SIZE);
-			if (read_return < 0)
-				return (NULL);
-			*(srcs + line_len + BUFFER_SIZE - 1) = '\0';
-			printf("new srcs : %s\n", srcs);
-		}
-		idx_srcs += BUFFER_SIZE;
-	}
+			line_len = 0;
+			p = srcs + idx_srcs;
+			is_line = ft_try_get_line_len(p, &line_len);
+			printf("lien_len : %zu\n", line_len);
+			res = NULL;
 
+			if (is_line == TRUE)
+			{
+				res = (char	*)malloc(sizeof(char) * line_len + idx_srcs);
+				ft_memcpy(res, srcs, line_len + idx_srcs);
+				idx_srcs = 0;
+				free(srcs);
+				break;
+			}
+
+			if (j == 0 && read(fd, srcs + (buffer_length / 2), BUFFER_SIZE) < 0)
+				return (NULL);
+
+			*(buffer + buffer_length) = '\0';
+			i++;	
+		}
+
+		if (is_line == TRUE)
+			break;
+		
+		rotation *= 2;
+		buffer_length = BUFFER_SIZE * rotation;
+		p = (char *)malloc(sizeof(char) * (buffer_length * 2) + 1);
+		ft_memcpy(p, src, buffer_length / 2);
+		free(src);
+		src = p;
+
+	}	
+	
 	return (res);
 }
 
