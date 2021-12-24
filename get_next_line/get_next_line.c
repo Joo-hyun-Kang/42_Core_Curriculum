f97d517054f62b17fdea2395a0fa3506fd0aaab3
DESKTOP-37M41BA\johuyn <autoba9687@gmail.com>
2021-12-24 23:47:06 +0900

    get_next_line.c - refactoring

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -12,71 +12,71 @@
 
 #include "get_next_line.h"
 
+#include <stdio.h>
+
+#define BUFFER_SIZE 20
+
 char	*get_next_line(int fd)
 {
 	static char		*buffer;
+	static size_t	buffer_length;
 	static size_t	idx_buffer = 0;
-	char			*p;
 	char			*res;
-	size_t			line_len;
+	char			*p;
+	size_t			line_length;
 	int				is_line;
-	char			*tmp;
 	size_t			rotation;
-	size_t			buffer_length;
-	size_t			i;
 	
-	rotation = 1;
-	buffer_length = BUFFER_SIZE * rotation;
+	if (idx_buffer == 0)
+	{
+		rotation = 1;
+		buffer_length = BUFFER_SIZE * rotation;
+		buffer = (char *)malloc(sizeof(char) * (buffer_length + 1));
 
-	buffer = (char *)malloc(sizeof(char) * (buffer_length * 2) + 1));
-	if (read(fd, buffer, buffer_length) < 0)
-		return (NULL);
-	*(buffer + buffer_length) = '\0';
+		if (ft_read_str_fd(buffer, fd, buffer_length) == 0)
+			return (NULL);
+	}
 
 	is_line = FALSE;
+	res = NULL;
 
 	while(TRUE)
 	{
-		i = 0;
-		while (i < 2)
-		{
-			p = buffer + idx_buffer;
-			is_line = ft_try_get_line_len(p, &line_len);
-			printf("lien_len : %zu\n", line_len);
-			res = NULL;
-
-			if (is_line == TRUE)
-			{
-				res = (char	*)malloc(sizeof(char) * line_len + idx);
-				ft_memcpy(res, buffer, line_len + idx);
-				idx = 0;
-				free(srcs);
-				break;
-			}
-
-			if (i == 0 && read(fd, srcs + (buffer_length / 2), BUFFER_SIZE) < 0)
-				return (NULL);
-
-			*(buffer + buffer_length * 2) = '\0';
-			i++;	
-		}
+		p = buffer + idx_buffer;
+		is_line = ft_try_get_line_len(p, &line_length);
+		idx_buffer = line_length;
+		printf("lien_len : %zu\n", line_length);
 
 		if (is_line == TRUE)
+		{
+			res = ft_strdup_range_malloc(buffer, 0, line_length + idx_buffer);
+
+			p = NULL;
+			if (idx_buffer == buffer_length)
+				idx_buffer = 0;
+			else
+				p = ft_strdup_range_malloc(buffer, idx_buffer, buffer_length);
+
+			free(buffer);
+			buffer = p;
+
 			break;
+		}
 		
 		rotation++;
-		buffer_length = BUFFER_SIZE * ft_power(2, rotation);
-		p = (char *)malloc(sizeof(char) * (buffer_length * 2) + 1);
+		buffer_length = BUFFER_SIZE * rotation;
+		p = (char *)malloc(sizeof(char) * (buffer_length + 1));
 		ft_memcpy(p, buffer, buffer_length / 2);
 		free(buffer);
 		buffer = p;
-		*(buffer + buffer_length) = '\0';
-
+		if (ft_read_str_fd(buffer + idx_buffer, fd, buffer_length / 2) < 0)
+			return (NULL);
 	}	
 	
 	return (res);
 }
 
+/*
 unsigned int	ft_power(unsigned int base, unsigned int n)
 {
 	if (n == 0)
@@ -84,6 +84,7 @@ unsigned int	ft_power(unsigned int base, unsigned int n)
 
 	return base * ft_power(base, n - 1);
 }
+*/
 
 int	ft_try_get_line_len(char *srcs, size_t *out_len)
 {
@@ -103,4 +104,29 @@ int	ft_try_get_line_len(char *srcs, size_t *out_len)
 	}
 	*out_len = tmp - srcs + 1;
 	return (is_get_line);
+}
+
+int main(void)
+{
+	int		fd;
+	char	*p;
+
+	fd = open("source.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		printf("FILE_OPEN ERROR");
+		return 0;
+	}
+	
+	p = get_next_line(fd);
+
+	while (p != NULL)
+	{
+		printf("%s", p);
+		free(p);
+		p = get_next_line(fd);
+	}
+
+
+	return 1;
 }
