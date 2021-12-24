b6e70518fd46f07a6c4b85c09ab6a2a275ca5c37
Joohyun Kang <jokang@c10r8s6.42seoul.kr>
2021-12-24 14:59:32 +0900

    get_next_line.c - refactoring

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jokang <autoba9687@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 16:10:11 by jokang            #+#    #+#             */
-/*   Updated: 2021/12/21 17:44:49 by jokang           ###   ########.fr       */
+/*   Updated: 2021/12/24 14:58:40 by jokang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 char	*get_next_line(int fd)
 {
 	static char		*buffer;
-	static size_t	idx = 0;
+	static size_t	idx_buffer = 0;
 	char			*p;
 	char			*res;
 	size_t			line_len;
@@ -24,61 +24,65 @@ char	*get_next_line(int fd)
 	size_t			rotation;
 	size_t			buffer_length;
 	size_t			i;
-	//줄바꿈이 나올 때까지 read한다
-		//static은 배열의 2배로 잡는다
-	//줄바꿈이 나오면 return 한다
-	//EOF가 나오면 return 한다
-	//줄바꿈이 나오지 않으면 다시 나머지 버퍼를 채운다
-	//없으면 반복한다
 	
 	rotation = 1;
-	is_line = FALSE;
 	buffer_length = BUFFER_SIZE * rotation;
+
 	buffer = (char *)malloc(sizeof(char) * (buffer_length * 2) + 1));
 	if (read(fd, buffer, buffer_length) < 0)
 		return (NULL);
 	*(buffer + buffer_length) = '\0';
+
+	is_line = FALSE;
 
 	while(TRUE)
 	{
 		i = 0;
 		while (i < 2)
 		{
-			line_len = 0;
-			p = srcs + idx_srcs;
+			p = buffer + idx_buffer;
 			is_line = ft_try_get_line_len(p, &line_len);
 			printf("lien_len : %zu\n", line_len);
 			res = NULL;
 
 			if (is_line == TRUE)
 			{
-				res = (char	*)malloc(sizeof(char) * line_len + idx_srcs);
-				ft_memcpy(res, srcs, line_len + idx_srcs);
-				idx_srcs = 0;
+				res = (char	*)malloc(sizeof(char) * line_len + idx);
+				ft_memcpy(res, buffer, line_len + idx);
+				idx = 0;
 				free(srcs);
 				break;
 			}
 
-			if (j == 0 && read(fd, srcs + (buffer_length / 2), BUFFER_SIZE) < 0)
+			if (i == 0 && read(fd, srcs + (buffer_length / 2), BUFFER_SIZE) < 0)
 				return (NULL);
 
-			*(buffer + buffer_length) = '\0';
+			*(buffer + buffer_length * 2) = '\0';
 			i++;	
 		}
 
 		if (is_line == TRUE)
 			break;
 		
-		rotation *= 2;
-		buffer_length = BUFFER_SIZE * rotation;
+		rotation++;
+		buffer_length = BUFFER_SIZE * ft_power(2, rotation);
 		p = (char *)malloc(sizeof(char) * (buffer_length * 2) + 1);
-		ft_memcpy(p, src, buffer_length / 2);
-		free(src);
-		src = p;
+		ft_memcpy(p, buffer, buffer_length / 2);
+		free(buffer);
+		buffer = p;
+		*(buffer + buffer_length) = '\0';
 
 	}	
 	
 	return (res);
+}
+
+unsigned int	ft_power(unsigned int base, unsigned int n)
+{
+	if (n == 0)
+		return 1;
+
+	return base * ft_power(base, n - 1);
 }
 
 int	ft_try_get_line_len(char *srcs, size_t *out_len)
