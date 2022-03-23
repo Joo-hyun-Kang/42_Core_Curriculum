fa638c0d7c0a53daea0f17dbf1d998344e51c4cc
home <autoba9687@gmail.com>
2022-03-24 08:01:52 +0900

    ft_printf spilt src, header

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,20 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+
+# define true (1)
+# define false (0)
+
+int     ft_printf(const char *format, ...);
+void    ft_put_pointer(void *pointer, int *out_count);
+void    ft_putInt_hex(int num, int isUpper, int *out_count);
+
+void    ft_putnbr_unsigned(unsigned int n, int* out_count);
+void	ft_putnbr(int n, int* out_count);
+void	ft_putstr(char *s, int *out_count);
+void	putnbr_recursive(unsigned int n, int is_neg, int* out_count);
+
+
+#endif /* FT_PRINTF_H */
---
+++ ft_printf/ft_printf_utils.c
@@ -0,0 +1,63 @@
+#include "ft_printf.h"
+
+void	ft_putstr(char *s, int *out_count)
+{
+	while (*s != '\0')
+	{
+		write(1, s, 1);
+        (*out_count)++;
+		s++;
+	}
+}
+
+void	putnbr_recursive(unsigned int n, int is_neg, int* out_count)
+{
+	char	ch;
+
+	if (is_neg == 1)
+	{
+		is_neg = 0;
+		write(1, "-", 1);
+        (*out_count)++;
+	}
+	if (n > 9)
+	{
+		putnbr_recursive(n / 10, is_neg, out_count);
+	}
+	n %= 10;
+	ch = n + '0';
+	write(1, &ch, 1);
+    (*out_count)++;
+}
+
+void	ft_putnbr(int n, int* out_count)
+{
+	int				is_negative;
+	unsigned int	n_abs;
+
+	is_negative = 0;
+	if (n < 0)
+	{
+		is_negative = 1;
+		n_abs = (unsigned int)((n + 1) * -1 + 1);
+	}
+	else
+	{
+		n_abs = (unsigned int)n;
+	}
+	putnbr_recursive(n_abs, is_negative, out_count);
+}
+
+void ft_putnbr_unsigned(unsigned int n, int* out_count)
+{
+	char	ch;
+
+	if (n > 9)
+	{
+		ft_putnbr_unsigned(n / 10, out_count);
+	}
+	n %= 10;
+	ch = n + '0';
+	write(1, &ch, 1);
+    (*out_count)++;
+}
