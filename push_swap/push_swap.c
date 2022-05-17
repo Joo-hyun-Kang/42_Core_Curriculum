8861442983e0765f2645e5229b90f7c48b53aa9e
jokang <autoba9687@gmail.com>
2022-05-17 20:54:37 +0900

    project implement finish

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -1,33 +1,5 @@
 #include "push_swap.h"
 
-static char	**ft_do_split_malloc_or_null(const char *s, char c, char **out_res);
-static int	get_w_length(const char *str, char c);
-static int	is_charset(char str, char c);
-static void	do_free_malloc(char **res, char **tmp_res);
-char	**ft_split(const char *s, char c);
-
-static int	ft_isspace(char ch);
-int	ft_atoi(const char *str, int *out_res);
-int	ft_isdigit(int c);
-
-size_t	ft_strlen(const char *s);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-
-void print_error_exit();
-
-int add_arraylist(arraylist_t *arraylist, int value);
-int is_arraylist_full(arraylist_t *arraylist);
-int allocate_arraylist(arraylist_t *arraylist);
-void free_arraylist(arraylist_t *arraylist);
-void is_parse_argv_malloc(int argc, char **argv, arraylist_t *arraylist);
-
-void get_optiml_pivot(arraylist_t *arraylist, stack_t *stack);
-void quick_sort(int start, int end, arraylist_t *arraylist, int *is_eailer_sorted);
-int divdie_and_conquer(int left, int right, int *nums);
-void swap(int *nums, int a, int b);
-int	is_overlap(arraylist_t *arraylist);
-
 ///////라이브러리 가져와서 쓰는 함수////////
 
 char	**ft_split(const char *s, char c) 
@@ -308,35 +280,42 @@ int main(int argc, char** argv)
 	ft_stack_t pa_stack_a;
 	ft_stack_t pa_stack_b;
 	ft_stack_t pa_pivot;
+	ft_stack_t pa_out_queue;
 
     assert(allocate_arraylist(&pa_arraylist) == true);
     is_parse_argv_malloc(argc, argv, &pa_arraylist);
 	get_optiml_pivot(&pa_arraylist, &pa_pivot);
+	ft_set_stack(&pa_arraylist, &pa_stack_a, true);
+	ft_set_stack(&pa_arraylist, &pa_stack_b, false);
+	free_arraylist(&pa_arraylist);
+	ft_sort(&pa_stack_a, &pa_stack_b, &pa_pivot, &pa_out_queue);
 
 	for (int i = 0; i < pa_arraylist.length; i++) {
 		printf("%d\n", pa_arraylist.pa_arr[i]);
 	}
 
-	// 최적 피봇 구하기
-	// 팝 인큐 디큐 구현
-	// sa, sb 등 구현
-	// sort하기
+	printf("\n");
+
+	linkedlist_t *temp = pa_pivot.front;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->value);
+		temp = temp->next;
+	}
+
+	ft_free(&pa_stack_a);
+	ft_free(&pa_stack_b);
+	ft_free(&pa_pivot);
+	temp = pa_out_queue.front;
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->value);
+		temp = temp->next;
+	}
 	
-
-
-	free_arraylist(&pa_arraylist);
-	//free_stack
-	//free_pivot
+	ft_free(&pa_out_queue);
+	//제출 전 어서트 날리기
 }
-//내 로직에서 체크할 것
-//-빈칸이 있는 경우
-//"    1" 4 2
-
-// pivot을 따로 stack에서 빼고 pivot을 구하셈
-// 어째든 둘 다 링크스 리스트에서 스택에서 추가하고 빼는 걸 구현해야 함
-// 넣어야 할 것은 스택에 새로운 노드를 추가하는 것
-// pivot에 새로운 노드를 추가하는 것
-// 나중에 sort 할 때 옮기는 것 -> 추가하고 빼는 걸 이용한다
 
 // 함수의 선조건
 // 1. 할당된 arraylist에 parsing된 데이터가 있어야 함
@@ -345,7 +324,7 @@ int main(int argc, char** argv)
 // 1. 입력 데이터가 중복된 숫자가 들어왔을 떄 Error 출력하고 종료
 // 2. 입력 데이터가 정렬된 거 일 때 아무것도 출력하지 않고 그냥 종료
 // 3. stack->pivot의 최적의 피봇을 linked list로 반환
-void get_optiml_pivot(arraylist_t *arraylist, ft_stack_t *pivot) 
+void	get_optiml_pivot(arraylist_t *arraylist, ft_stack_t *pivot) 
 {
 	int *p = arraylist->pa_arr;
 	int len = arraylist->length;
@@ -357,9 +336,22 @@ void get_optiml_pivot(arraylist_t *arraylist, ft_stack_t *pivot)
 	// 예외처리 5: 정렬된 거 일 때 아무것도 출력하지 않고 그냥 종료
 	if (is_eailer_sorted)
 		exit(0);
-	// 최적의 피봇 구하기
-	// 	
+	get_optimal_pivot_recursive(arraylist, pivot, 0, arraylist->length - 1);
 
+}
+
+void	get_optimal_pivot_recursive(arraylist_t *arraylist, ft_stack_t *stack, int start, int end)
+{
+	int	mid;
+
+	if (end - start <= 2)
+		return ;
+
+	mid = (start + end) / 2;
+	ft_enqueue(stack, arraylist->pa_arr[mid]);
+	
+	get_optimal_pivot_recursive(arraylist, stack, mid + 1, end);
+	get_optimal_pivot_recursive(arraylist, stack, start, mid - 1);
 }
 
 int	is_overlap(arraylist_t *arraylist)
@@ -470,44 +462,109 @@ void is_parse_argv_malloc(int argc, char **argv, arraylist_t *pa_arraylist)
 	}
 }
 
-void push_stack(ft_stack_t *stack, int content)
-{
-	linkedlist_t *new_lst;
-	
-	if (stack->top == NULL) 
-	{
-		stack->top = ft_lstnew_malloc(content);
-		stack->bottom = stack->top;
-		stack->size++;
-	}
-	new_lst = ft_lstnew_malloc(content);
-	new_lst->next = stack->root;
-	stack->top = new_lst;
-	stack->size++;
-}
-
-int pop_stack(ft_stack_t *stack)
-{
-	int value;
-	
-	value = stack->top->value;
-	//POCU lab2 Remove 코드 참고해
-
-}
-
 void print_error_exit()
 {
 	write(2, "Error\n", 6);
 	exit(0);
 }
 
-//push_front 검증 필!
-void push_front(linkedlist_t **root, int content)
+void ft_push(ft_stack_t *stack, int content)
 {
-	linkedlist_t	*new;
+	linkedlist_t *new_lst;
+	
+	if (stack->size == 0) 
+	{
+		stack->front = ft_lstnew_malloc(content);
+		stack->back = stack->front;
+	}
+	else
+	{
+		new_lst = ft_lstnew_malloc(content);
+		new_lst->next = stack->front;
+		stack->front = new_lst;
+	}
+	stack->size++;
+}
 
-	new = ft_lstnew_malloc(content);
-	ft_lstadd_front(root, new);
+void ft_enqueue(ft_stack_t *stack, int content)
+{
+	if (stack->size == 0)
+	{
+		stack->front = ft_lstnew_malloc(content);
+		stack->back = stack->front;
+	}
+	else
+	{
+		stack->back->next = ft_lstnew_malloc(content);
+		stack->back = stack->back->next;
+	}
+	stack->size++;
+}
+
+int	ft_pop_front(ft_stack_t *stack)
+{
+	int	result;
+
+	assert(stack->size != 0);
+	result = stack->front->value;
+	stack->front = ft_remove(stack);
+	stack->size--;
+
+	if (stack->size == 0)
+	{
+		stack->back = NULL;
+	}
+
+	return (result);
+}
+
+int	ft_pop_back(ft_stack_t *stack)
+{
+	int				result;
+	int				i;
+	linkedlist_t	*prev_back;
+
+	assert(stack->size != 0);
+	if (stack->size == 1)
+	{
+		return (ft_pop_front(stack));
+	}
+	i = 1;
+	prev_back = stack->front;
+	while (i < stack->size - 1)
+	{
+		prev_back = prev_back->next;
+		i++;
+	}
+	result = stack->back->value;
+	free(stack->back);
+	stack->back = prev_back;
+	stack->size--;
+	return (result);
+}
+
+int ft_peak(ft_stack_t *stack)
+{
+	int result;
+	
+	assert(stack->size != 0);
+	result = stack->front->value;
+	return (result);
+}
+
+linkedlist_t *ft_remove(ft_stack_t *rootOrNull)
+{
+	linkedlist_t	*result;
+	
+	if (rootOrNull->size == 0)
+	{
+		return (rootOrNull->front);
+	}
+	result = rootOrNull->front->next;
+	rootOrNull->front->next = NULL;
+	free(rootOrNull->front);
+	rootOrNull->front = NULL;
+	return (result);
 }
 
 linkedlist_t	*ft_lstnew_malloc(int content)
@@ -519,30 +576,349 @@ linkedlist_t	*ft_lstnew_malloc(int content)
 		return (NULL);
 	head->value = content;
 	head->next = NULL;
-	head->prev = NULL;
 	return (head);
 }
 
-void	ft_lstadd_front(linkedlist_t **lst, linkedlist_t *new)
+void ft_free(ft_stack_t *stack)
 {
-	if (*lst == NULL)
+	linkedlist_t	*temp;
+
+	while (stack->front != NULL)
 	{
-		*lst = new;
+		temp = stack->front->next;
+		free(stack->front);
+		stack->front = temp;
+		stack->size--;
+	}
+	//assert(stack->size == 0);
+	stack->front = NULL;
+	stack->back = NULL;
+}
+
+void ft_sa(ft_stack_t *a, ft_stack_t *queue)
+{
+	int	top;
+	int	second_top;
+
+	top = ft_pop_front(a);
+	second_top = ft_pop_front(a);
+	ft_push(a, top);
+	ft_push(a, second_top);
+	ft_enqueue(queue, (int)COMMAND_SA);
+}
+
+void ft_sb(ft_stack_t *b, ft_stack_t *queue)
+{
+	int	top;
+	int	second_top;
+
+	top = ft_pop_front(b);
+	second_top = ft_pop_front(b);
+	ft_push(b, top);
+	ft_push(b, second_top);
+	ft_enqueue(queue, (int)COMMAND_SB);
+}
+
+void ft_ss(ft_stack_t *a, ft_stack_t *b, ft_stack_t *queue)
+{
+	ft_sa(a, queue);
+	ft_sb(b, queue);
+	ft_enqueue(queue, (int)COMMAND_SS);
+}
+
+void ft_ra(ft_stack_t *a, ft_stack_t *queue)
+{
+	int	top;
+
+	top = ft_pop_front(a);
+	ft_enqueue(a, top);
+	ft_enqueue(queue, (int)COMMAND_RA);
+}
+
+void ft_rb(ft_stack_t *b, ft_stack_t *queue)
+{
+	int	top;
+
+	top = ft_pop_front(b);
+	ft_enqueue(b, top);
+	ft_enqueue(queue, (int)COMMAND_RB);
+}
+
+void ft_rr(ft_stack_t *a, ft_stack_t *b, ft_stack_t *queue)
+{
+	ft_ra(a, queue);
+	ft_rb(b, queue);
+	ft_enqueue(queue, (int)COMMAND_RR);
+}
+
+void ft_rra(ft_stack_t *a, ft_stack_t *queue)
+{
+	int	bottom;
+
+	bottom = ft_pop_back(a);
+	ft_push(a, bottom);
+	ft_enqueue(queue, (int)COMMAND_RRA);
+}
+
+void ft_rrb(ft_stack_t *b, ft_stack_t *queue)
+{
+	int	bottom;
+
+	bottom = ft_pop_back(b);
+	ft_push(b, bottom);
+	ft_enqueue(queue, (int)COMMAND_RRB);
+}
+
+void ft_rrr(ft_stack_t *a, ft_stack_t *b, ft_stack_t *queue)
+{
+	ft_rra(a, queue);
+	ft_rrb(b, queue);
+	ft_enqueue(queue, (int)COMMAND_RRR);
+}
+
+void ft_pa(ft_stack_t *a, ft_stack_t *b, ft_stack_t *queue)
+{
+	int top_b;
+
+	top_b = ft_pop_front(b);
+	ft_push(a, top_b);
+	ft_enqueue(queue, (int)COMMAND_PA);
+}
+
+void ft_pb(ft_stack_t *a, ft_stack_t *b, ft_stack_t *queue)
+{
+	int top_a;
+
+	top_a = ft_pop_front(a);
+	ft_push(b, top_a);
+	ft_enqueue(queue, (int)COMMAND_PB);
+}
+
+void ft_set_stack(arraylist_t *arraylist, ft_stack_t *stack, int is_stack_a)
+{
+	int	i;
+	int *p;
+
+	stack->front = NULL;
+	stack->back = NULL;
+	stack->size = 0;
+	p = arraylist->pa_arr;
+	i = 0;
+	if (is_stack_a)
+	{
+		while (i < arraylist->length)
+		{
+			ft_push(stack, *p);
+			p++;
+			i++;
+			stack->size++;
+		}
+	}
+}
+
+void ft_sort(ft_stack_t *a, ft_stack_t *b, ft_stack_t *pivot, ft_stack_t *pa_out_queue)
+{
+	int circle;
+	
+	circle = 0;
+	if (a->size == 2)
+	{
+		swap_size_two_a(a, pa_out_queue);
+	}
+	else if (a->size == 3)
+	{
+		swap_size_three_a(a, pa_out_queue);
 	}
 	else
 	{
-		new->next = *lst;
-		
-		*lst = new;
+		ft_split_a_to_b(a, b, pivot, pa_out_queue, a->size);
 	}
 }
 
-void push_back(linkedlist_t **end, int content)
+void swap_size_two_a(ft_stack_t *stack, ft_stack_t *queue)
 {
-	linkedlist_t	*new;
+	int	top;
+	int	sec_top;
 
-	new = ft_lstnew_malloc(content);
-	ft_lstadd_front(end, new);
+	top = ft_pop_front(stack);
+	sec_top = ft_pop_front(stack);
+
+	if (top > sec_top)
+	{
+		ft_push(stack, sec_top);
+		ft_push(stack, top);
+		ft_sa(stack, queue);
+	}
+	else
+	{
+		ft_push(stack, sec_top);
+		ft_push(stack, top);
+	}
 }
 
-///////////
+void swap_size_three_a(ft_stack_t *a, ft_stack_t *queue)
+{
+	int	arr[3];
+	int	sort_order;
+
+	arr[0] = ft_pop_front(a);
+	arr[1] = ft_pop_front(a);
+	arr[2] = ft_pop_front(a);
+	if (arr[0] > arr[1])
+		if (arr[0] > arr[2])
+		{
+			if (arr[1] > arr[2])
+				sort_order = THREE_NUM_ORDER_CASE2;
+			else
+				sort_order = THREE_NUM_ORDER_CASE3;
+		}
+		else
+			sort_order = THREE_NUM_ORDER_CASE1;	
+	else
+		if (arr[0] < arr[2] && arr[1] > arr[2])
+			sort_order = THREE_NUM_ORDER_CASE4;
+		else
+			sort_order = THREE_NUM_ORDER_CASE5;
+	swap_three_num_a(a, sort_order, queue);
+}
+
+void swap_three_num_a(ft_stack_t *stack, int sort_order, ft_stack_t *queue)
+{
+	if (sort_order == THREE_NUM_ORDER_CASE1)
+	{
+		ft_sa(stack, queue);
+	}
+	else if (sort_order == THREE_NUM_ORDER_CASE2)
+	{
+		ft_sa(stack, queue);
+		ft_rra(stack, queue);
+	}
+	else if (sort_order == THREE_NUM_ORDER_CASE3)
+	{
+		ft_ra(stack, queue);
+	}
+	else if (sort_order == THREE_NUM_ORDER_CASE4)
+	{
+		ft_sa(stack, queue);
+		ft_ra(stack, queue);
+	}
+	else if (sort_order == THREE_NUM_ORDER_CASE5)
+	{
+		ft_rra(stack, queue);
+	}
+}
+
+void swap_size_two_b(ft_stack_t *stack, ft_stack_t *queue)
+{
+	int	top;
+	int	sec_top;
+
+	top = ft_pop_front(stack);
+	sec_top = ft_pop_front(stack);
+
+	if (top > sec_top)
+	{
+		ft_push(stack, sec_top);
+		ft_push(stack, top);
+	}
+	else
+	{
+		ft_push(stack, sec_top);
+		ft_push(stack, top);
+		ft_sb(stack, queue);
+	}
+}
+
+void ft_split_a_to_b(ft_stack_t *a, ft_stack_t *b, ft_stack_t *pivots, ft_stack_t *queue, int stack_a_count)
+{
+	int	i;
+	int	ra_count;
+	int	pb_count;
+	int	pivot;
+
+	if (a->size <- 1)
+		return;
+	if (a->size == 2)
+	{
+		swap_size_two_a(a, queue);
+		return;
+	}
+	if (a->size == 3)
+	{
+		swap_size_three_a(a, queue);
+		return;
+	}
+	i = 0;
+	ra_count = 0;
+	pb_count = 0;
+	pivot = ft_pop_front(pivots);
+	while (i < stack_a_count)
+	{
+		if (ft_peak(a) > pivot)
+		{
+			ft_ra(a, queue);
+			ra_count++;
+		}
+		else
+		{
+			ft_pb(a, b, queue);
+			pb_count++;
+		}
+		i++;
+	}
+	i = ra_count;
+	while (i >= 0)
+	{
+		ft_rra(a, queue);
+		i--;
+	}
+	ft_split_a_to_b(a, b, pivots, queue, ra_count);
+	ft_split_b_to_a(a, b, pivots, queue, pb_count);
+}
+
+void ft_split_b_to_a(ft_stack_t *a, ft_stack_t *b, ft_stack_t *pivots, ft_stack_t *queue, int count)
+{
+	int	i;
+	int	rb_count;
+	int	pa_count;
+	int	pivot;
+
+	if (count == 1)
+	{
+		ft_pa(a, b, queue);
+		return ;
+	}
+	if (count == 2)
+	{
+		swap_size_two_b(b, queue);
+		ft_pa(a, b, queue);
+		ft_pa(a, b, queue);
+		return ;
+	}
+	i = 0;
+	rb_count = 0;
+	pa_count = 0;
+	pivot = ft_pop_front(pivots);
+	while (i < count)
+	{
+		if (ft_peak(b) > pivot)
+		{
+			ft_rb(a, queue);
+			rb_count++;
+		}
+		else
+		{
+			ft_pa(a, b, queue);
+			pa_count++;
+		}
+		i++;
+	}
+	i = rb_count;
+	while (i >= 0)
+	{
+		ft_rrb(a, queue);
+		i--;
+	}
+	ft_split_a_to_b(a, b, pivots, queue, pa_count);
+	ft_split_b_to_a(a, b, pivots, queue, rb_count);
+}
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -17,14 +17,35 @@ typedef struct arraylist {
 typedef struct linkedlist {
 	int value;
 	struct linkedlist *next;
-	struct linkedlist *prev;
 } linkedlist_t;
 
 typedef struct ft_stack {
-	linkedlist_t *top;
-	linkedlist_t *bottom;
+	linkedlist_t *front;
+	linkedlist_t *back;
 	size_t size;
 } ft_stack_t;
+
+enum COMMAND {
+	COMMAND_SA,
+	COMMAND_SB,
+	COMMAND_SS,
+	COMMAND_RA,
+	COMMAND_RB,
+	COMMAND_RR,
+	COMMAND_RRA,
+	COMMAND_RRB,
+	COMMAND_RRR,
+	COMMAND_PA,
+	COMMAND_PB
+};
+
+enum THREE_NUM_ORDER {
+	THREE_NUM_ORDER_CASE1,
+	THREE_NUM_ORDER_CASE2,
+	THREE_NUM_ORDER_CASE3,
+	THREE_NUM_ORDER_CASE4,
+	THREE_NUM_ORDER_CASE5
+};
 
 static char	**ft_do_split_malloc_or_null(const char *s, char c, char **out_res);
 static int	get_w_length(const char *str, char c);
@@ -40,16 +61,47 @@ size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 
-void print_error_exit();
-
 int add_arraylist(arraylist_t *arraylist, int value);
 int is_arraylist_full(arraylist_t *arraylist);
 int allocate_arraylist(arraylist_t *arraylist);
 void free_arraylist(arraylist_t *arraylist);
-void is_parse_argv_malloc(int argc, char **argv, arraylist_t *arraylist);
 
-void get_optiml_pivot(arraylist_t *arraylist, stack_t *stack);
-void quick_sort(int start, int end, arraylist_t *arraylist, int *is_eailer_sorted);
-int divdie_and_conquer(int left, int right, int *nums);
-void swap(int *nums, int a, int b);
+void	get_optiml_pivot(arraylist_t *arraylist, ft_stack_t *pivot);
+void	get_optimal_pivot_recursive(arraylist_t *arraylist, ft_stack_t *stack, int start, int end);
 int	is_overlap(arraylist_t *arraylist);
+void	quick_sort(int start, int end, arraylist_t *arraylist, int *is_eailer_sorted);
+int	divdie_and_conquer(int left, int right, int *nums);
+void	swap(int *nums, int a, int b);
+
+void	is_parse_argv_malloc(int argc, char **argv, arraylist_t *arraylist);
+void	print_error_exit();
+
+void ft_push(ft_stack_t *stack, int content);
+void ft_enqueue(ft_stack_t *stack, int content);
+int	ft_pop_front(ft_stack_t *stack);
+int	ft_pop_back(ft_stack_t *stack);
+int ft_peak(ft_stack_t *stack);
+linkedlist_t *ft_remove(ft_stack_t *rootOrNull);
+linkedlist_t	*ft_lstnew_malloc(int content);
+void ft_free(ft_stack_t *stack);
+
+void ft_sa(ft_stack_t *a, ft_stack_t *queue);
+void ft_sb(ft_stack_t *b, ft_stack_t *queue);
+void ft_ss(ft_stack_t *a, ft_stack_t *b, ft_stack_t *queue);
+void ft_ra(ft_stack_t *a, ft_stack_t *queue);
+void ft_rb(ft_stack_t *b, ft_stack_t *queue);
+void ft_rr(ft_stack_t *a, ft_stack_t *b, ft_stack_t *queue);
+void ft_rra(ft_stack_t *a, ft_stack_t *queue);
+void ft_rrb(ft_stack_t *b, ft_stack_t *queue);
+void ft_rrr(ft_stack_t *a, ft_stack_t *b, ft_stack_t *queue);
+void ft_pa(ft_stack_t *a, ft_stack_t *b, ft_stack_t *queue);
+void ft_pb(ft_stack_t *a, ft_stack_t *b, ft_stack_t *queue);
+
+void ft_set_stack(arraylist_t *arraylist, ft_stack_t *stack, int is_stack_a);
+void ft_sort(ft_stack_t *a, ft_stack_t *b, ft_stack_t *pivot, ft_stack_t *pa_out_queue);
+void swap_size_two_a(ft_stack_t *stack, ft_stack_t *queue);
+void swap_size_three_a(ft_stack_t *a, ft_stack_t *queue);
+void swap_three_num_a(ft_stack_t *stack, int sort_order, ft_stack_t *queue);
+void swap_size_two_b(ft_stack_t *stack, ft_stack_t *queue);
+void ft_split_a_to_b(ft_stack_t *a, ft_stack_t *b, ft_stack_t *pivots, ft_stack_t *queue, int stack_a_count);
+void ft_split_b_to_a(ft_stack_t *a, ft_stack_t *b, ft_stack_t *pivots, ft_stack_t *queue, int count);
